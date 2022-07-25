7575a1605e5ea34bb01cd6a2e5e083647787c0d4
NareshChippa <109961259+NareshChippa@users.noreply.github.com>
2022-07-25 21:40:27 +0530

    Added Logger in Main file

--- MySpringBpptApp/src/main/java/com/example/demo/MySpringBpptAppApplication.java
+++ MySpringBpptApp/src/main/java/com/example/demo/MySpringBpptAppApplication.java
@@ -1,16 +1,29 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySpringBpptAppApplication {
+	private static Logger logger = LoggerFactory.getLogger(MySpringBpptAppApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application init started....");
+	}
 
 	public static void main(String[] args) {
+		logger.info("Application main method started");
 		SpringApplication.run(MySpringBpptAppApplication.class, args);
 		System.out.println("Hi I am here");
 		System.out.println("Added one more print statement");
 		System.out.println("Added one more print statement");
+		logger.info("Application main method finished");
 	}
 
 }
+
