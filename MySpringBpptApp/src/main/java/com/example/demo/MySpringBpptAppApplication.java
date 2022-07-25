64b6c8b92c0c4a91298c6547c5602590213ed5d0
NareshChippa <109961259+NareshChippa@users.noreply.github.com>
2022-07-25 18:04:55 +0530

    Add files via upload

---
+++ MySpringBpptApp/src/main/java/com/example/demo/MySpringBpptAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBpptAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBpptAppApplication.class, args);
+		System.out.println("Hi I am here");
+	}
+
+}
