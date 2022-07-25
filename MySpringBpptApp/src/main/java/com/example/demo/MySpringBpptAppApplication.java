0d8266730a331f5c5852f44252d298d86fa92b7d
NareshChippa <109961259+NareshChippa@users.noreply.github.com>
2022-07-25 19:45:59 +0530

    Update MySpringBpptAppApplication.java

--- MySpringBpptApp/src/main/java/com/example/demo/MySpringBpptAppApplication.java
+++ MySpringBpptApp/src/main/java/com/example/demo/MySpringBpptAppApplication.java
@@ -9,6 +9,7 @@ public class MySpringBpptAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringBpptAppApplication.class, args);
 		System.out.println("Hi I am here");
+		System.out.println("Added one more print statement");
 	}
 
 }
